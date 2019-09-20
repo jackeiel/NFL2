7bd1f513a394b043f481492f2be5212948857ac1
Jack Eiel <eielj@Jacks-MacBook-Air.local>
2019-09-20 18:38:50 -0400

    gather outline

---
+++ data_cleaning/gather_results.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# what I'd like to do is simply build a df with:
+# home_team
+# away_team
+# predicted scores
+# predicted over under
+# vegas line
+# vegas over under
+# actual scores
+# actual over under
+# bet on who?
+# win bet?
+
+# basically this is a scraper and compiler
+# need to find a good site to pull lines / over under from...
